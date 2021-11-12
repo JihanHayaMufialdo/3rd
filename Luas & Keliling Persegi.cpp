98c97328bc90cbd1227a8b852b6bc38e34e76b2e
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-12 11:43:19 +0700

    Add files via upload

---
+++ Luas & Keliling Persegi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int s;
+	
+	cout << "\n------ PERSEGI ------\n\n";
+	cout << "sisi (m) : ";
+	cin  >> s; 
+	
+	cout << "Keliling : " << 4*s << " m\n";
+	cout << "Luas\t : " << s*s << " m^2";
+}
