d9eea6f64f7cfac1dba49b5b9d0aa4fae36e9662
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-12 22:31:55 +0700

    LUAS & KELILING BANGUN DATAR - IF

--- Luas & Keliling Belah Ketupat.cpp
+++ Luas & Keliling Belah Ketupat.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main (){
+	
 	float s,d1,d2;
 	
 	cout << "\n----- BELAH KETUPAT -----\n\n";
@@ -12,7 +13,11 @@ int main (){
 	cout << "diagonal 2 (m) : ";
 	cin  >> d2;
 	
+	if(s!=0){
 	cout << "\nKeliling : " << 4*s<< " m\n";
+	}
+	
+	if(d1!=0 && d2!=0){
 	cout << "Luas\t : " << (d1*d2)/2 << " m^2";
-
+	}
 }
