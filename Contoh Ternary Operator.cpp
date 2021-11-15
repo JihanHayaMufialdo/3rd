4d4aa182163d62f76835dc51ffb9d54e349252fc
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-15 23:52:44 +0700

    Program TERNARY

--- Contoh Ternary Operator.cpp
+++ Contoh Ternary Operator.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 int main(){
 
-    int jawaban1,jawaban2;
-    string hasil1, hasil2;
+    int jawaban1,jawaban2,jawaban3;
+    string hasil1, hasil2,hasil3;
 
-    cout << "3+4 = ";
+    cout << "3 + 4 = ";
     cin >> jawaban1;
     hasil1 = (jawaban1 == 7) ? "Benar" : "Salah";
     cout << "Jawaban Anda " << hasil1 << endl;
     
-    cout << "3*4 = ";
+    cout << "3 * 4 = ";
     cin >> jawaban2;
     hasil2 = (jawaban2 == 12) ? "Benar" : "Salah";
     cout << "Jawaban Anda " << hasil2 << endl;
+    
+    cout << "10 % 3 = ";
+    cin >> jawaban3;
+    hasil3 = (jawaban3 == 1) ? "Benar" : "Salah";
+    cout << "Jawaban Anda " << hasil3 << endl;
     
     
 
