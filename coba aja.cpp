3a581717c8971c30ae7ab2bca798c58b68cd0a2e
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-30 23:19:44 +0700

    Update coba aja.cpp

--- coba aja.cpp
+++ coba aja.cpp
@@ -2,25 +2,4 @@
 using namespace std;
 int main(){
 	
-	string bintang[10] =
-	{
-	"*",
-	" *",
-	"  *",
-	"   *",
-	"    *",
-	"     *",
-	"      *",
-	"       *",
-	"        *",
-	"         *",
-	};
 	
-	for(int i=0; i<10; i++){
-		for(int j=0; j<10; j++){
-			cout << bintang[i];
-		}
-		cout << "\n";
-	}
-}
-
